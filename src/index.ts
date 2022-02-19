b8105de1cb085b6e09fb53535e2b4c78cfbe0cc0
Dmytro Furtas <dmytro.furtas07@gmail.com>
2022-02-19 19:25:46 +0200

    Added body parser

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Hello world'));
 
